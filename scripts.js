eefe30608718120d18662785cdd6a5ef2417b0db
spring2025tm <tmatten@hawk.iit.edu>
2025-04-15 18:19:55 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function initMap() {
   const center = { lat: 37.7749, lng: -122.4194 };
 
-  const map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     center: center,
     zoom: 12,
     mapTypeControl: true,
